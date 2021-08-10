f327225aa0d5a4d48521e408f1067422413643f5
Tomohiro Akiya <thairwars@gmail.com>
2021-08-10 23:42:04 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザー
+User.create!(
+  [
+    {
+      name:  "山田 良子",
+      email: "yamada@example.com",
+      password:              "foobar",
+      password_confirmation: "foobar",
+      admin: true
+    },
+    {
+      name:  "鈴木 恵子",
+      email: "suzuki@example.com",
+      password:              "password",
+      password_confirmation: "password",
+    },
+    {
+      name:  "採用 太郎",
+      email: "recruit@example.com",
+      password:              "password",
+      password_confirmation: "password",
+    },
+  ]
+)
+
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
